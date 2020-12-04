bc4f2559eb72e825baf34aa69780164e705bf3e5
Diego <diegoarlechuga@gmail.com>
2020-12-03 22:14:58 -0600

    Ultimos cambios

--- stickeron/settings.py
+++ stickeron/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'stickeron.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'stickeron',
-        'USER': 'postgres',
-        'PASSWORD': 'dWxD23Hq',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
